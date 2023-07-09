696c411e9898d63241fdd4f598490fae68f09aa9
Jorge Federico Miranda Morones <jorge100099@hotmail.com>
2023-07-09 14:43:40 -0600

    Creación del login

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
+
+
+    const {user}=useContext(AuthContext);
 
     const navigate =useNavigate();
 
@@ -51,7 +56,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
                     
                     <span className='nav-item nav-link text-info'>
-                        Jorge
+                        {user?.name }
                     </span>
 
                     <button className='nav-item nav-link btn' onClick={onLogout}>
